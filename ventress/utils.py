51f509394aaead126f426579fe2202d954066b39
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-02-14 15:40:05 -0800

    Edited JSONToClass to generalize better to different types of JSON responses

--- ventress/utils.py
+++ ventress/utils.py
@@ -10,21 +10,15 @@ class JSONToClass:
 
     async def _async_init(self):
         await self._setattrs()
+        if self._json is None:
+            return None
         return self
 
-    async def _setattrs(self):
+    async def _get_json(self):
         async with aiohttp.ClientSession() as session:
             async with session.post(self.url,
                                     json=self.payload,
                                     params=self.params if hasattr(self, 'params') else {}) as request:
                 self._request = request
-                self._json = await request.json()
-        if self._json is None:
-            self._raise_error_json_is_none()
-
-        decimal = re.compile(r'\d+\.\d+')
-        for key in self._json:
-            if decimal.fullmatch(str(self._json[key])):
-                setattr(self, key, float(self._json[key]))
-            else:
-                setattr(self, key, self._json[key])
+                return await request.json()
+        
