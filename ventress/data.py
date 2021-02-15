134fff9bb336aea835c3de41a8d5cc7a81515870
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-02-14 20:21:32 -0800

    Created UserMatchHistory class
    This makes it easy to retreive a user's match history.

--- ventress/data.py
+++ ventress/data.py
@@ -1,6 +1,7 @@
 import re
 
 from .utils import JSONToClass
+from dateutil.parser import isoparse
 
 class SurvivrData(JSONToClass):
     url = 'https://surviv.io/api/user_stats'
@@ -48,3 +49,44 @@ class SurvivrData(JSONToClass):
                         setattr(mode,
                                 f"{'_'.join([camel.match(key).group(0), *map(str.lower, match)])}",
                                 mode[key])
+
+class UserMatchHistory(JSONToClass):
+    url = 'https://surviv.io/api/match_history'
+    valid_team_modes = (1, 2, 4, 7)
+
+    def __init__(self, slug, count, offset=0, team_mode=7):
+        self.slug = slug.lower()
+        self.count = count
+        self.offset = offset
+        if team_mode not in self.valid_team_modes:
+            raise ValueError(f'{team_mode!r} is not a valid team mode.')
+        self.team_mode = team_mode
+        self.payload = {
+            'slug': self.slug,
+            'count': self.count,
+            'offset': self.offset,
+            'teamModeFilter': self.team_mode
+        }
+    
+    def __len__(self):
+        return len(self.games)
+    
+    def __getitem__(self, index):
+        return self.games[index]
+    
+    async def _setattrs(self):
+        self._json = await self._get_json()
+
+        if self._json:
+            class Game(dict): pass
+            self.games = [Game(game) for game in self._json]
+            for game in self.games:
+                for attr in game:
+                    # There are no decimals in this response
+                    # we do not not need to check for them
+
+                    # Try and convert to a datetime object
+                    try:
+                        setattr(game, attr, isoparse(game[attr]))
+                    except (ValueError, TypeError):
+                        setattr(game, attr, game[attr])
