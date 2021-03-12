14a737fec981be67c77e76a6555068713be388f4
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-03-11 18:58:49 -0800

    Added logic to handle if the survivr has not every played this game mode.

--- ventress/cogs/commands.py
+++ ventress/cogs/commands.py
@@ -5,7 +5,9 @@ class CommandsCog(commands.Cog):
     @commands.command(aliases=['stats'])
     async def user_stats(self, ctx, slug, gamemode='all', interval='all'):
         survivr = await SurvivrData(slug, interval=interval, gamemode=gamemode)
-        if survivr:
+        if survivr and survivr.games:
             await ctx.send(embed=survivr.embed)
+        elif survivr:
+            await ctx.send(f'**{slug}** has not played any games in the {survivr.gamemode.proper} gamemode.')
         else:
             await ctx.send(f'{slug!r} is not valid survivr.')
