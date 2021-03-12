7cb2fa964394ad85433e2d2a757caf6c7aae4c7e
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-03-11 18:06:10 -0800

    Add CommandsCog to bot when running it in __main__.py

--- ventress/__main__.py
+++ ventress/__main__.py
@@ -2,10 +2,13 @@ import os
 
 from dotenv import load_dotenv
 from discord.ext import commands
+from .cogs.commands import CommandsCog
 
 load_dotenv(override=True)
 
 bot = commands.Bot(command_prefix='test.', case_insensitive=True)
+
+bot.add_cog(CommandsCog())
 
 @bot.event
 async def on_ready():
